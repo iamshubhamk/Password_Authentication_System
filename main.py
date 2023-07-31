8c162a52945db478b6c121f5bf64141b081f7a47
iamshubhamk <69527654+iamshubhamk@users.noreply.github.com>
2023-07-31 04:31:01 +0000

    Created a Password Authenticator

---
+++ main.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"iamshubham": "123456", "shubham.k": "654321"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
